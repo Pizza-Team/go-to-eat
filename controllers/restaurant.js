de43b38fe6707edae407c6ba13acee5af904e0f5
noritaketakamichi <noritaket28@gmail.com>
2020-09-22 22:05:26 +0900

    data.json

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -12,6 +12,7 @@ const header = {
     "x-rapidapi-key": process.env.API_KEY,
     "useQueryString": true
 };
+
 router.get("/", async function(req, res) {
     console.log(111111111);
     const url = "https://gurunavi-restaurant-search.p.rapidapi.com/ForeignRestSearchAPI/v3/";
@@ -31,4 +32,23 @@ router.get("/", async function(req, res) {
 
 });
 
+//東京の駅を全て返すAPI
+router.get("/stationInTokyo", async function(req, res) {
+    console.log(111111111);
+    const url = "https://gurunavi-restaurant-search.p.rapidapi.com/ForeignRestSearchAPI/v3/";
+    const reqForRestaurant = unirest("GET", url);
+    reqForRestaurant.query({
+        "lang": "en",
+        "english_menu": "1"
+    });
+    reqForRestaurant.headers(header)
+
+    reqForRestaurant.end(function(resOfRestaurant) {
+        if (resOfRestaurant.error) throw new Error(resOfRestaurant.error);
+
+        // console.log(JSON.stringify(res.body));
+        res.send(resOfRestaurant.body);
+    });
+
+});
 module.exports = router;
