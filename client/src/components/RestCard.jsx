4c3f3d739454d25a724dfdd17c2eb90836c09d01
Catherine Nuguid <catnuguid@gmail.com>
2020-09-23 02:01:05 +0900

    add data to restaurant modal window

--- client/src/components/RestCard.jsx
+++ client/src/components/RestCard.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClock,
+  faMapMarkerAlt,
+  faPhone,
+  faUtensils,
+  faExternalLinkAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function RestCard({ restaurant }) {
   const [display, setDisplay] = useState(false);
@@ -24,8 +32,54 @@ export default function RestCard({ restaurant }) {
 
           <div className="checkout">
             <h1 className="checkout-name">{restaurant.name.name}</h1>
-            <div className="checkout-image">
-                <img src="https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg" />
+            <div className="informational">
+              <div className="checkout-image">
+                <img
+                  src={
+                    "https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg"
+                  }
+                />
+              </div>
+              <div className="checkout-info">
+                <div className="info-line">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faClock} size="lg" color="navy"/>
+                  </div>
+                  <div className="info-content">{restaurant.business_hour}</div>
+                </div>
+                <div className="info-line">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" color="navy"/>
+                  </div>
+                  <div className="info-content">
+                    {restaurant.contacts.address}
+                  </div>
+                </div>
+                <div className="info-line">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faPhone} size="lg" color="navy"/>
+                  </div>
+                  <div className="info-content">{restaurant.contacts.tel}</div>
+                </div>
+                <div className="info-line">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faUtensils} size="lg" color="navy"/>
+                  </div>
+                  <div className="info-content">
+                    {restaurant.categories.category_name_l[0]}
+                  </div>
+                </div>
+                <div className="info-line">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faExternalLinkAlt} size="lg" color="navy"/>
+                  </div>
+                  <div className="info-content">
+                    <a href={restaurant.url} target="_blank">
+                      Website
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </>
