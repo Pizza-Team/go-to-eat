68966742e9fd22dbd2ba48d8a9e9eee1f3449c38
Catherine Nuguid <catnuguid@gmail.com>
2020-09-23 16:06:34 +0900

    fix path

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
 );
 
 
-app.use(express.static(path.join(__dirname, "../client", + "/build")));
+app.use(express.static(path.join(__dirname, "../client", "/build")));
 
 app.use("/api", restaurantApi, payApi);
 
