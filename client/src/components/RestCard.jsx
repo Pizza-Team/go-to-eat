5fd461571734000d0fc7d15f56fedbbacd571e1e
Eri Koyano <erikoyano@MacBook-Pro-8.local>
2020-09-23 11:20:20 +0900

    added images

--- client/src/components/RestCard.jsx
+++ client/src/components/RestCard.jsx
@@ -1,35 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import images from '../../src/';
 
 export default function RestCard({ restaurant }) {
-  const [display, setDisplay] = useState(false);
+	const [display, setDisplay] = useState(false);
+	const [images, setImages] = useState();
 
-  return (
-    <>
-      <div id="restaurant-container" onClick={() => setDisplay(true)}>
-        <div className="image">
-          <img src="https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg" />
-        </div>
+	function imageMatch() {
+		restaurant.filter((food) => {
+			return restaurant.type === 'Japanese';
+		});
+	}
 
-        <div className="info">
-          <div className="name">{restaurant.name}</div>
+	return (
+		<>
+			<div id="restaurant-container" onClick={() => setDisplay(true)}>
+				<div className="image">
+					<img src="https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg" />
+				</div>
 
-          <div className="location">{restaurant.city}</div>
+				<div className="info">
+					<div className="name">{restaurant.name}</div>
 
-          <div className="type">{restaurant.type}</div>
-        </div>
-      </div>
-      {display && (
-        <>
-          <div className="main-overlay" onClick={() => setDisplay(false)}></div>
+					<div className="location">{restaurant.city}</div>
 
-          <div className="checkout">
-            <h1 className="checkout-name">{restaurant.name}</h1>
-            <div className="checkout-image">
-                <img src="https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg" />
-            </div>
-          </div>
-        </>
-      )}
-    </>
-  );
+					<div className="type">{restaurant.type}</div>
+				</div>
+			</div>
+			{display && (
+				<>
+					<div className="main-overlay" onClick={() => setDisplay(false)}></div>
+
+					<div className="checkout">
+						<h1 className="checkout-name">{restaurant.name}</h1>
+						<div className="checkout-image">
+							<img src="https://resizer.otstatic.com/v2/photos/wide-medium/1/25243676.jpg" />
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
 }
