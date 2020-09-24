ecbcbab4a46c5da9e7684449a2933991c015750e
yusuke99 <sendyusukeamail@gmail.com>
2020-09-24 22:12:56 +0900

    fix url

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -17,8 +17,8 @@ export default function Order({ restaurant, t }) {
       payment_method_types: ["card"],
       line_items: [],
       mode: 'payment',
-      success_url: `https://eatokyo.herokuapp.com/success`,
-      cancel_url: `https://eatokyo.herokuapp.com/cancel`
+      success_url: `${process.env.REACT_APP_DOMAIN}/success`,
+      cancel_url: `${process.env.REACT_APP_DOMAIN}/cancel`
 		};
 
     for (let i = 0; i < quantity.length; i++) {
