f08311176b1674838971bc5f806e8fc0eca4cdad
yusuke99 <sendyusukeamail@gmail.com>
2020-09-24 22:05:02 +0900

    fix

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -17,7 +17,7 @@ export default function Order({ restaurant, t }) {
       payment_method_types: ["card"],
       line_items: [],
       mode: 'payment',
-      success_url: `http://localhost:3000/success`,
+      success_url: `https://eatokyo.herokuapp.com/success`,
       cancel_url: `https://eatokyo.herokuapp.com/cancel`
 		};
 
