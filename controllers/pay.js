576916fc3761ce5cd8785f2e67254493fa705a24
yusuke99 <sendyusukeamail@gmail.com>
2020-09-24 21:47:45 +0900

    fix route

--- controllers/pay.js
+++ controllers/pay.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.use(express.static('.'));
+// router.use(express.static("../client/html/"));
 
 router.post('/pay/create-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create(req.body);
