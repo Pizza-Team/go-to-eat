9481524cd8a163268e593beee95f5cbe4f0e89f2
yusuke99 <sendyusukeamail@gmail.com>
2020-09-24 21:12:41 +0900

    fix: route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Success from "./components/Success";
 import "./App.css";
 import Home from "./components/Home";
+// import Order from "./components/Order";
 
 export default function App() {
   return (
@@ -14,11 +16,25 @@ export default function App() {
       {/* <Route path="/success" component={Success} /> */}
 
 
-      <Route path="/success" />
+      <Route path="/success.html" />
         <Success />
       <Route />
 
       </div>
     </BrowserRouter>
+
+    // <Router>
+    //   <Route path="/" />
+    //     <Home />
+    //   <Route />
+    //   <Switch>
+    //     <Route path="/success.html">
+    //       <Success />
+    //     </Route>
+    //     <Route path="/pay/create-session">
+    //       <Order />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
