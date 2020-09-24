86169d6409f1f1879308795dadaea0e483e870dd
Catherine Nuguid <catnuguid@gmail.com>
2020-09-25 00:28:19 +0900

    update path to client

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "../client", "/build")));
 app.use("/api", restaurantApi, payApi);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = app;
